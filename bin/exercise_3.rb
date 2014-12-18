59623bfc20a4f3687d0d8af51ea01fd20ea7b537
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-12-17 21:54:28 -0700

    finished 3rd exercise took out what didn't work

--- bin/exercise_3.rb
+++ bin/exercise_3.rb
@@ -2,22 +2,13 @@ require_relative '../data/crm'
 require 'pp'
 
 
-result = []
-CRM[:people].each do |person|
-  if person[:employments].empty?
-    result << person.tap {|person| person.delete(:employments)}
-  end
-end
-pp result
-
-
 def unemployed(crm)
   unemployed_array = []
   crm[:people].each do |person|
     if person[:employments].empty?
       person_hash = {id: person[:id], first_name: person[:first_name],
         last_name: person[:last_name]}
-        
+
       unemployed_array << person_hash
     end
   end
