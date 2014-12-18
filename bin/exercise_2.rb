ca86b6311aa110e908cefdbc936c8e4ae4ad4e03
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-12-17 19:00:01 -0700

    finished exercise 2

--- bin/exercise_2.rb
+++ bin/exercise_2.rb
@@ -1,48 +1,47 @@
 require_relative '../data/crm'
 require 'pp'
 
-employment_list = []
+result = []
 
 CRM[:people].each do |person|
 
-    if person[:employments] != []
-
   person[:employments].each do |employment|
-     employment_info = {   
-      [:person_id]
-      [:person_first_name]
-      [:person_last_name]
-      [:title]}
+    employment_info = {}
+    employment_info[:company_id] = employment[:company_id]
+    company = CRM[:companies].find {|company| company [:id] == employment[:company_id]}
 
-  CRM[:companies].each do |company|
+    employment_info = {company_name: company[:name], person_id: person[:id],
+      person_first_name: person[:first_name], person_last_name: person[:last_name],
+      title: employment[:title]}
 
-    if employment[:company_id] == company[:id]
-      employment_info[:company_id] = company[:id]
-      emplyment_info[:company_name] = company[:name]
+      result << employment_info
     end
+  end
+  pp result
+  
 
-## Challenge #2 - all employments
-
-Write some code that will return an array of all the employments,
-including the person name, company name, person id, company id
-and title.
-
-```ruby
-[
-  {
-    :company_id => 4,
-    :company_name => "Nicolas and Sons",
-    :person_id => 20,
-    :person_first_name => "Savannah",
-    :person_last_name => "Clementina",
-    :title => "Chief Communications Consultant"
-  },
-  {
-    :company_id => 6,
-    :company_name => "Mueller LLC",
-    :person_id => 20,
-    :person_first_name => "Elyse",
-    :person_last_name => "Jensen",
-    :title => "Human Directives Engineer"
-  },
-]
+  # ## Challenge #2 - all employments
+  #
+  # Write some code that will return an array of all the employments,
+  # including the person name, company name, person id, company id
+  # and title.
+  #
+  # ```ruby
+  # [
+  #   {
+  #     :company_id => 4,
+  #     :company_name => "Nicolas and Sons",
+  #     :person_id => 20,
+  #     :person_first_name => "Savannah",
+  #     :person_last_name => "Clementina",
+  #     :title => "Chief Communications Consultant"
+  #   },
+  #   {
+  #     :company_id => 6,
+  #     :company_name => "Mueller LLC",
+  #     :person_id => 20,
+  #     :person_first_name => "Elyse",
+  #     :person_last_name => "Jensen",
+  #     :title => "Human Directives Engineer"
+  #   },
+  # ]
